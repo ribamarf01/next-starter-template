c24318e9efdb13ab3758d75bc6bae69a090ec327
ribamarf01 <ribamar.f01@gmail.com>
2022-06-17 12:51:24 -0300

    Starter Template

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (<>
+    <div className="h-screen w-full grid place-items-center text-7xl text-white bg-gradient-to-tr from-red-400 to-purple-600 font-bold">Hello Starter!</div>
+  </>)
+}
+
+export default Home
